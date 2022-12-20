2a9cf2cb71ac8e9eb355dc4bf260c8a950694484
adamw7 <adamwitkowski3@gmail.com>
2022-12-20 17:21:01 +0100

    Introducing ZipB class

---
+++ examples/src/main/java/io/github/adamw7/ZipB.java
@@ -0,0 +1,36 @@
+package io.github.adamw7;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipB {
+
+	public static void main(String[] args) throws IOException {
+		contruct(100000000L);
+	}
+
+	private static void contruct(long len) throws IOException {
+		long start = System.currentTimeMillis();
+		File file = new File("b.zip");
+		file.delete();
+		try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(file))) {
+			ZipEntry entry = new ZipEntry("a");
+	        entry.setComment("Created for fun");
+	        zout.putNextEntry(entry);
+			
+			for (int i = 0; i < len; ++i) {
+				zout.write(createBatch());
+			}
+		}
+
+		System.out.println("Zip entry contains: " + len + " bytes");
+		System.out.println("Took " + (System.currentTimeMillis() - start)/1000 + " secs");
+	}
+
+	private static byte[] createBatch() {
+		return new byte[] { 'a' };
+	}
+}
