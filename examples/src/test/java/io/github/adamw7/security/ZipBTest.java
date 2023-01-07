9a3e6fde80fdc9ff8759675b990dc5897d06ae2d
adamw7 <adamwitkowski3@gmail.com>
2023-01-07 12:23:33 +0100

    Adding more asserts in tests and cleanup

--- examples/src/test/java/io/github/adamw7/security/ZipBTest.java
+++ examples/src/test/java/io/github/adamw7/security/ZipBTest.java
@@ -2,20 +2,31 @@ package io.github.adamw7.security;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ZipBTest {
+
+	private static final String FILE_NAME = "b.zip";
 
 	@ParameterizedTest
 	@ValueSource(ints = {100, 1000, 10000})
 	public void happyPath(int len) {
 		try {
 			ZipB.contruct(len);
+			assertTrue(new File(FILE_NAME).exists());
 		} catch (IOException e) {
 			fail(e.getMessage());
 		}
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		new File(FILE_NAME).delete();
 	}
 }
